0ff96083cdd8fbc667633f8c4f9dd5e0e42b0a23
Sherry Ladzinski <sherryladzinski@gmail.com>
2015-02-07 22:10:20 -0500

    able to persist post data to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-
+		if current_user
+    		@post = Post.new(post_params)
+    		@post.user_id = @current_user.id
+    		@post.location_id = params[:location_id].to_i
+      			if @post.save
+        			redirect_to location_path(@post.location_id)
+      			else
+        			flash[:notice]="You must select a rating to submit a review"
+        			redirect_to(:back)
+      			end
+    	else
+    		redirect_to root_path
+    	end
 	end
 
 
 private
-  def post
+  def post_params
     params.require(:post).permit(:name, :contact, :about_me, :housing_preference)
   end
 
